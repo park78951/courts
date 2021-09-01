3598cf39f1d467545ad665ec82d14a85dbb88b11
박상규 <park78951@gmail.com>
2021-09-01 14:32:53 +0900

    feat: typeorm 설정 코드 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 import { MorganModule, MorganInterceptor } from 'nest-morgan';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [MorganModule],
+  imports: [
+    MorganModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'sangq',
+      password: process.env.DB_PW,
+      database: 'court_finder',
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
